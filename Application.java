18f77d282cdd9376bbbd80961e34a0c40903ef0d
Lori <palfalvi.lorand@gmail.com>
2017-02-13 15:47:37 +0200

    Deleted the verification for int.max. and added arraylist to collection.

--- Application.java
+++ Application.java
@@ -9,7 +9,7 @@ public class Application {
     private String arrayListProblem() {
 
         ArrayList<Integer> returnedList = new ArrayList<Integer>();
-        Collection<Integer> listOfArrayList = new ArrayList<Integer>();
+        Collection listOfArrayList = new ArrayList<Integer>();
         Scanner input = new Scanner(System.in);
         double numberFromInput = 0.0;
         String value = " ";
@@ -23,110 +23,43 @@ public class Application {
                 for (int index = 0; index < numbersAsStrings.length; index++) {
                     if (numbersAsStrings[index].equals("quit")) {
                         value = numbersAsStrings[index];
+                        Arrays.fill(numbersAsStrings, null);
                         break;
                     } else {
-                        numberFromInput = Double.parseDouble(numbersAsStrings[index]);
-                        if (numberFromInput > Integer.MAX_VALUE) {
-                            System.out.println("Please enter a smaller number: ");
-                            counterOfLine--;
-                            break;
-                        } else {
-                            returnedList = insertingNumbers(numbersAsStrings);
-                        }
+                        returnedList = insertingNumbersInArrayList(numbersAsStrings);
                     }
                 }
-                for (int index = 0; index < returnedList.size(); index++) {
+
+                /*for (int index = 0; index < returnedList.size(); index++) {
                     System.out.print(returnedList.get(index) + " ");
 
-                }
+                }*/
                 System.out.println();
                 counterOfLine++;
             } catch (NumberFormatException ex) {
                 System.out.println("You have entered a number that is too big, or a string of characters. \n" +
                         "Please enter the numbers for the line again.");
             }
+
+            listOfArrayList.add(returnedList);
         }
 
-        /*
-        for (int index = 0; index < returnedList.size(); index++) {
-            System.out.print(returnedList.get(index) + " ");
+        listOfArrayList.remove(listOfArrayList.size() - 1);
+        System.out.println();
+        System.out.println(" -- The collection --\n");
+
+
+        for (Iterator it = listOfArrayList.iterator(); it.hasNext(); ) {
+            System.out.print(it.next() + "\n");
         }
-        */
-
-
-
-        /*
-        for (Iterator it = listOfArrayList.iterator(); it.hasNext();)
-        {
-            System.out.print(it.next());
-        }
-        */
-
-        /*while (input.next() == "y")
-        {
-            System.out.format("Enter the numbers for line %d: ", counterOfLine);
-            String[] numbersAsStrings = input.nextLine().split("\\s");
-            System.out.println("E BREAK");
-
-            for (int index = 0; index < numbersAsStrings.length; index++) {
-                numberFromInput = Double.parseDouble(numbersAsStrings[index]);
-                // System.out.println(numberFromInput);
-                if (numberFromInput > Integer.MAX_VALUE) {
-                    System.out.print("Please enter a smaller number: ");
-                    counterOfLine -= 2;
-                    break;
-                } else
-                    lineNumbers.add(Integer.parseInt(numbersAsStrings[index]));
-            }
-            counterOfLine++;
-        }*/
-
-
-        // String[] numbersAsStrings = input.nextLine().split("\\s");
-        // lineNumbers.add(numberOfIntegersInALine);
-
-        // String[] numbersAsStrings = input.nextLine().split("\\s");
-        //for (int index = 0; index < numbersAsStrings.length; index++) {
-        // numberFromInput = Double.parseDouble(numbersAsStrings[index]);
-        // System.out.println(numberFromInput);
-           /* if (numberFromInput > Integer.MAX_VALUE) {
-                System.out.print("Please enter a smaller number: ");
-                counterOfLine -= 2;
-                break;
-            } else*/
-        //      lineNumbers.add(Integer.parseInt(numbersAsStrings[index]));
-        //}
-
-
-
-        /*while (input.hasNextInt()) {
-            counterOfLine++;
-            System.out.format("Enter the numbers for line %d: ", counterOfLine);
-            String[] numbersAsStrings = input.nextLine().split("\\s");
-            System.out.println("E BREAK ");
-
-            for (int index = 0; index < numbersAsStrings.length; index++) {
-                numberFromInput = Double.parseDouble(numbersAsStrings[index]);
-               // System.out.println(numberFromInput);
-                if (numberFromInput > Integer.MAX_VALUE) {
-                    System.out.print("Please enter a smaller number: ");
-                    counterOfLine -= 2;
-                    break;
-                } else
-                    lineNumbers.add(Integer.parseInt(numbersAsStrings[index]));
-            }
-        }
-*/
-
 
         return "";
     }
 
-    private ArrayList<Integer> insertingNumbers(String[] numbers) {
+    private ArrayList<Integer> insertingNumbersInArrayList(String[] numbers) {
         ArrayList<Integer> lineNumbers = new ArrayList<Integer>();
 
         for (int index = 0; index < numbers.length; index++) {
-
             lineNumbers.add(Integer.parseInt(numbers[index]));
         }
         lineNumbers.add(0, lineNumbers.size());
