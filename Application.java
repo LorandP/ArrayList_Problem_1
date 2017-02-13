ca9a0227083a2526e654e17c8728b813ecbaa6fa
Lori <palfalvi.lorand@gmail.com>
2017-02-13 17:20:19 +0200

    Changed the while condition, added conditin if the string does not contain a number.

--- Application.java
+++ Application.java
@@ -6,134 +6,63 @@ import java.util.*;
  */
 public class Application {
 
-    private String arrayListProblem() {
+    private void arrayListProblem() {
 
         ArrayList<Integer> returnedList = new ArrayList<Integer>();
-        Collection<Integer> listOfArrayList = new ArrayList<Integer>();
+        Collection listOfArrayList = new ArrayList<Integer>();
         Scanner input = new Scanner(System.in);
-        double numberFromInput = 0.0;
-        String value = " ";
         int counterOfLine = 1;
+        int size = 0;
 
-        while (!value.equals("quit")) {
+        System.out.print("Enter the amount of lines that you want to add: ");
+        size = input.nextInt();
+        input.nextLine();
+        System.out.println();
+        String[] numbersAsStrings = new String[size];
+
+        while (counterOfLine <= size) {
             try {
-                System.out.format("Enter the numbers for line - %d - or type 'quit' to exit: ", counterOfLine);
-                String[] numbersAsStrings = input.nextLine().split("\\s");
+                System.out.format("Enter the numbers for line %d : ", counterOfLine);
+                numbersAsStrings = input.nextLine().split("\\s");
 
                 for (int index = 0; index < numbersAsStrings.length; index++) {
-                    if (numbersAsStrings[index].equals("quit")) {
-                        value = numbersAsStrings[index];
-                        break;
+                    if (numbersAsStrings[index].matches("[0-9]+")) {
+                        returnedList = insertingNumbersInArrayList(numbersAsStrings);
                     } else {
-                        numberFromInput = Double.parseDouble(numbersAsStrings[index]);
-                        if (numberFromInput > Integer.MAX_VALUE) {
-                            System.out.println("Please enter a smaller number: ");
-                            counterOfLine--;
-                            break;
-                        } else {
-                            returnedList = insertingNumbers(numbersAsStrings);
-                        }
+                        System.out.format("You have entered a string of characters. \n" +
+                                "Please enter the numbers for line %d again.", counterOfLine);
+                        counterOfLine--;
+                        break;
                     }
-                }
-                for (int index = 0; index < returnedList.size(); index++) {
-                    System.out.print(returnedList.get(index) + " ");
-
                 }
                 System.out.println();
                 counterOfLine++;
             } catch (NumberFormatException ex) {
-                System.out.println("You have entered a number that is too big, or a string of characters. \n" +
-                        "Please enter the numbers for the line again.");
+                System.out.format("You have entered a number that is too big. \n" +
+                        "Please enter the numbers for line %d again.", counterOfLine);
             }
+
+            listOfArrayList.add(returnedList);
         }
-
-        /*
-        for (int index = 0; index < returnedList.size(); index++) {
-            System.out.print(returnedList.get(index) + " ");
-        }
-        */
-
-
-
-        /*
-        for (Iterator it = listOfArrayList.iterator(); it.hasNext();)
-        {
-            System.out.print(it.next());
-        }
-        */
-
-        /*while (input.next() == "y")
-        {
-            System.out.format("Enter the numbers for line %d: ", counterOfLine);
-            String[] numbersAsStrings = input.nextLine().split("\\s");
-            System.out.println("E BREAK");
-
-            for (int index = 0; index < numbersAsStrings.length; index++) {
-                numberFromInput = Double.parseDouble(numbersAsStrings[index]);
-                // System.out.println(numberFromInput);
-                if (numberFromInput > Integer.MAX_VALUE) {
-                    System.out.print("Please enter a smaller number: ");
-                    counterOfLine -= 2;
-                    break;
-                } else
-                    lineNumbers.add(Integer.parseInt(numbersAsStrings[index]));
-            }
-            counterOfLine++;
-        }*/
-
-
-        // String[] numbersAsStrings = input.nextLine().split("\\s");
-        // lineNumbers.add(numberOfIntegersInALine);
-
-        // String[] numbersAsStrings = input.nextLine().split("\\s");
-        //for (int index = 0; index < numbersAsStrings.length; index++) {
-        // numberFromInput = Double.parseDouble(numbersAsStrings[index]);
-        // System.out.println(numberFromInput);
-           /* if (numberFromInput > Integer.MAX_VALUE) {
-                System.out.print("Please enter a smaller number: ");
-                counterOfLine -= 2;
-                break;
-            } else*/
-        //      lineNumbers.add(Integer.parseInt(numbersAsStrings[index]));
-        //}
-
-
-
-        /*while (input.hasNextInt()) {
-            counterOfLine++;
-            System.out.format("Enter the numbers for line %d: ", counterOfLine);
-            String[] numbersAsStrings = input.nextLine().split("\\s");
-            System.out.println("E BREAK ");
-
-            for (int index = 0; index < numbersAsStrings.length; index++) {
-                numberFromInput = Double.parseDouble(numbersAsStrings[index]);
-               // System.out.println(numberFromInput);
-                if (numberFromInput > Integer.MAX_VALUE) {
-                    System.out.print("Please enter a smaller number: ");
-                    counterOfLine -= 2;
-                    break;
-                } else
-                    lineNumbers.add(Integer.parseInt(numbersAsStrings[index]));
-            }
-        }
-*/
-
-
-        return "";
     }
 
-    private ArrayList<Integer> insertingNumbers(String[] numbers) {
+    private ArrayList<Integer> insertingNumbersInArrayList(String[] numbers) {
         ArrayList<Integer> lineNumbers = new ArrayList<Integer>();
 
         for (int index = 0; index < numbers.length; index++) {
-
             lineNumbers.add(Integer.parseInt(numbers[index]));
         }
         lineNumbers.add(0, lineNumbers.size());
         return lineNumbers;
     }
 
-    private void queryingTheArrayList(ArrayList<Integer> listOfNumbers) {
+    private void queryTheLines(Collection listOfLines) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("To query the lines, enter the line and the position you want to query from.");
+
+
     }
 
     public static void main(String[] args) {
